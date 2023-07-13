0ddbb4562f69df16947f5255b0d67a596e81df0c
alejandro <alejandrober96@gmail.com>
2023-07-13 12:46:05 -0500

    Add user controller with create and show methods

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,25 @@
+class API::V1:UsersController < ApplicationController
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: {status: 'SUCCESS', message: 'Saved user', data: user}, status: :ok
+    else
+      render json: {status: 'ERROR', message: 'User not saved', data: user.errors}, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    user = User.where(username: params[:username]).first
+    if User
+      render json: {status: 'SUCCESS', message: 'Loaded user', data: user}, status: :ok
+    else
+      render json: {status: 'ERROR', message: 'User not found', data: user.errors}, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:username, :full_name)
+  end
+end
