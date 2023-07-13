744df2639ba78eb31ca929f7206c97ecd7e28135
alejandro <alejandrober96@gmail.com>
2023-07-13 13:26:22 -0500

    linters

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,27 +2,27 @@ class Api::V1::UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
-      render json: {status: 'SUCCESS', message: 'Saved user', data: user}, status: :ok
+      render json: { status: 'SUCCESS', message: 'Saved user', data: user }, status: :ok
     else
-      render json: {status: 'ERROR', message: 'User not saved', data: user.errors}, status: :unprocessable_entity
+      render json: { status: 'ERROR', message: 'User not saved', data: user.errors }, status: :unprocessable_entity
     end
   end
 
   def show
     user = User.where(username: params[:username]).first
     if User
-      render json: {status: 'SUCCESS', message: 'Loaded user', data: user}, status: :ok
+      render json: { status: 'SUCCESS', message: 'Loaded user', data: user }, status: :ok
     else
-      render json: {status: 'ERROR', message: 'User not found', data: user.errors}, status: :unprocessable_entity
+      render json: { status: 'ERROR', message: 'User not found', data: user.errors }, status: :unprocessable_entity
     end
   end
 
   def destroy
     user = User.where(username: params[:username]).first
     if user.destroy
-      render json: {status: 'SUCCESS', message: 'Deleted user', data: user}, status: :ok
+      render json: { status: 'SUCCESS', message: 'Deleted user', data: user }, status: :ok
     else
-      render json: {status: 'ERROR', message: 'User not deleted', data: user.errors}, status: :unprocessable_entity
+      render json: { status: 'ERROR', message: 'User not deleted', data: user.errors }, status: :unprocessable_entity
     end
   end
 
