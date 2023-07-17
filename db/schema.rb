ed9c1993e0d0eb7c6fce2cf2859b6d6a8f6d673b
Luis Pomare <luispomare92@gmail.com>
2023-07-16 23:30:00 -0500

    Create migration to add table level reservations belongs_to car relationship

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_16_183433) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_17_042455) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,6 +30,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_16_183433) do
     t.float "cost"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "car_id", null: false
+    t.index ["car_id"], name: "index_reservations_on_car_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -39,4 +41,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_16_183433) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "reservations", "cars"
 end
