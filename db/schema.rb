91db807bdc7c68c47621d8f0db77c0f0da074def
Luis Pomare <luispomare92@gmail.com>
2023-07-17 00:01:41 -0500

    Create migration to add table level reservations belongs_to user relationship

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_17_042455) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_17_043514) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,7 +31,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_17_042455) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "car_id", null: false
+    t.bigint "user_id", null: false
     t.index ["car_id"], name: "index_reservations_on_car_id"
+    t.index ["user_id"], name: "index_reservations_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -42,4 +44,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_17_042455) do
   end
 
   add_foreign_key "reservations", "cars"
+  add_foreign_key "reservations", "users"
 end
