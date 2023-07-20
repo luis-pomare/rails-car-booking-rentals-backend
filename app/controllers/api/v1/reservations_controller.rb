94836e906037432985099dfccc0c4cbfb358db33
alejandro <alejandrober96@gmail.com>
2023-07-20 11:26:36 -0500

    Modifications on user model and controller, modification on reservation controller

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -1,11 +1,9 @@
 class Api::V1::ReservationsController < ApplicationController
   def index
     reservations = Reservation.includes(:car).where(user: User.find(params[:user_id]))
-    if reservations.nil?
+    render json: { status: 'SUCCESS', message: 'Loaded all reservations', data: reservations }, status: :ok
+    rescue ActiveRecord::RecordNotFound
       render json: { status: 'ERROR', message: 'This user dont have reservations' }, status: :unprocessable_entity
-    else
-      render json: { status: 'SUCCESS', message: 'Loaded all reservations', data: reservations }, status: :ok
-    end
   end
 
   def create
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
   validates :full_name, presence: true
-  has_many :reservations
+  has_many :reservations, dependent: :destroy
 end
