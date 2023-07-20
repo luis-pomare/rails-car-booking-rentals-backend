440142e4934a5e81851ab1221a92efb00f917d74
alejandro <alejandrober96@gmail.com>
2023-07-20 10:22:31 -0500

    Add modifications on cars controller

--- app/controllers/api/v1/cars_controller.rb
+++ app/controllers/api/v1/cars_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::CarsController < ApplicationController
   def index
     cars = Car.all
-    if cars.nil?
+    if cars.empty?
       render json: { status: 'ERROR', message: 'Cars not found' }, status: :unprocessable_entity
     else
       render json: { status: 'SUCCESS', message: 'Loaded all cars', data: cars }, status: :ok
