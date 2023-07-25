3f903a2b42e94e48ec55a6e8deed2f62849ff299
alejandro <alejandrober96@gmail.com>
2023-07-25 09:39:28 -0500

    Add dependent destroy to the reservations on cars model

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,5 +1,5 @@
 class Car < ApplicationRecord
-  has_many :reservations
+  has_many :reservations, dependent: :destroy
 
   validates :name, presence: true
   validates :cost_per_day, presence: true, numericality: { greater_than: 0 }
